ead929013dec31062b5188d6bba104372b3f1077
MLXProjects <maicolinux4@gmail.com>
2020-11-23 20:34:36 -0300

    Made dialog dynamic (header update)

--- include/aroma/ui/dialog.h
+++ include/aroma/ui/dialog.h
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *			http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * limitations under the License.
  *______________________________________________________________________________
  *
- * Filename		: window.h
+ * Filename    : window.h
  * Description : window api
  *
  * + This is part of libaroma, an embedded ui toolkit.
@@ -22,43 +22,53 @@
  *
  */
 #ifndef __libaroma_aroma_h__
-	#error "Include <aroma.h> instead."
+  #error "Include <aroma.h> instead."
 #endif
 #ifndef __libaroma_dialog_h__
 #define __libaroma_dialog_h__
 
-#define LIBAROMA_DIALOG_DIM_PARENT		0x01
-#define LIBAROMA_DIALOG_WITH_SHADOW	 0x02
+#define LIBAROMA_DIALOG_DIM_PARENT    0x01
+#define LIBAROMA_DIALOG_WITH_SHADOW   0x02
 #define LIBAROMA_DIALOG_ACCENT_BUTTON 0x04
-#define LIBAROMA_DIALOG_CANCELABLE		0x08
+#define LIBAROMA_DIALOG_CANCELABLE    0x08
 
 typedef struct{
-	/* window position */
-	int x;
-	int y;
-	int w;
-	int h;
-	byte flags;
-	LIBAROMA_CANVASP bg;
+  /* window position */
+  int x;
+  int y;
+  int w;
+  int h;
+  byte flags;
+  LIBAROMA_CANVASP bg;
 } LIBAROMA_DIALOG_DATA,
-	* LIBAROMA_DIALOG_DATAP;
+  * LIBAROMA_DIALOG_DATAP;
 
+typedef struct{
+	byte type;
+	int id;
+	char title[128];
+	char message[128];
+	LIBAROMA_CANVASP image;
+	int imageheight;
+	byte selected;
+	word flags;
+} LIBAROMA_LISTITEM_TEMPLATE;
 
 LIBAROMA_WINDOWP libaroma_dialog_window(
-	int w, int h, byte flags, LIBAROMA_COLORSETP colorset
+  int w, int h, byte flags, LIBAROMA_COLORSETP colorset
 );
 
 byte libaroma_dialog_free(
-	LIBAROMA_WINDOWP win
+  LIBAROMA_WINDOWP win
 );
 
 int libaroma_dialog_confirm(
-	const char * title,
-	const char * message,
-	const char * button1,
-	const char * button2,
-	LIBAROMA_COLORSETP colorset,
-	byte flags
+  const char * title,
+  const char * message,
+  const char * button1,
+  const char * button2,
+  LIBAROMA_COLORSETP colorset,
+  byte flags
 );
-	
+  
 #endif /* __libaroma_dialog_h__ */
