5dea589f336f37262822b131cb2ddfa9addf93fc
MLXProjects <maicolinux4@gmail.com>
2020-12-04 20:18:15 -0300

    Update deploy scripts with adb wait-for-device/recovery

--- tools/win32/deploy_android.bat
+++ tools/win32/deploy_android.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 cls
-cd bin
-adb wait-for-device
 if "%2"=="r" GOTO run
 call build_app.bat %1
 :run
+echo Waiting for device...
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c mount -o rw,remount /
 %LIBAROMA_ADB% shell su -c mkdir /tmp
-%LIBAROMA_ADB% push %1 /sdcard/
-%LIBAROMA_ADB% push ..\..\..\examples\%1\zip\res.zip /sdcard/
+%LIBAROMA_ADB% push bin\%1 /sdcard/
+%LIBAROMA_ADB% push ..\..\examples\%1\zip\res.zip /sdcard/
 %LIBAROMA_ADB% shell su -c cp /sdcard/%1 /tmp/
 %LIBAROMA_ADB% shell su -c chmod 755 /tmp/%1
 %LIBAROMA_ADB% shell su -c killall -19 system_server
@@ -18,4 +18,3 @@ call build_app.bat %1
 %LIBAROMA_ADB% shell su -c sleep 1
 %LIBAROMA_ADB% shell su -c killall -18 surfaceflinger
 %LIBAROMA_ADB% shell su -c killall -18 system_server
-cd ..
--- tools/win32/deploy_recovery.bat
+++ tools/win32/deploy_recovery.bat
@@ -3,8 +3,10 @@ cls
 if "%2"=="r" GOTO run
 call build_app.bat %1
 :run
+echo Waiting for device (recovery)
+%LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% push bin\%1 /tmp
-%LIBAROMA_ADB% push ..\..\..\examples\%1\zip\res.zip /sdcard/
+%LIBAROMA_ADB% push ..\..\examples\%1\zip\res.zip /sdcard/
 %LIBAROMA_ADB% shell chmod 755 /tmp/%1
 %LIBAROMA_ADB% shell killall -19 recovery
 %LIBAROMA_ADB% shell /tmp/%1 0 0 /sdcard/res.zip
