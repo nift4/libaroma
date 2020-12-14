f1214cadaa0fd757512fc6884ded110c7330b20b
MLXProjects <maicolinux4@gmail.com>
2020-12-14 18:21:42 -0300

    added more adb wait calls

--- tools/win32/deploy_android.bat
+++ tools/win32/deploy_android.bat
@@ -6,15 +6,23 @@ call build_app.bat %1
 echo Waiting for device...
 %LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c mount -o rw,remount /
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c mkdir /tmp
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% push bin\%1 /sdcard/
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% push ..\..\examples\%1\zip\res.zip /sdcard/
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c cp /sdcard/%1 /tmp/
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c chmod 755 /tmp/%1
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c killall -19 system_server
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c killall -19 surfaceflinger
-%LIBAROMA_ADB% shell su -c sleep 1
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c /tmp/%1
-%LIBAROMA_ADB% shell su -c sleep 1
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c killall -18 surfaceflinger
+%LIBAROMA_ADB% wait-for-device
 %LIBAROMA_ADB% shell su -c killall -18 system_server
--- tools/win32/deploy_recovery.bat
+++ tools/win32/deploy_recovery.bat
@@ -6,8 +6,13 @@ call build_app.bat %1
 echo Waiting for device (recovery)
 %LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% push bin\%1 /tmp
+%LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% push ..\..\examples\%1\zip\res.zip /sdcard/
+%LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% shell chmod 755 /tmp/%1
+%LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% shell killall -19 recovery
+%LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% shell /tmp/%1 0 0 /sdcard/res.zip
+%LIBAROMA_ADB% wait-for-recovery
 %LIBAROMA_ADB% shell killall -18 recovery
