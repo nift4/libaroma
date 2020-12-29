5e56b87d6bbed125a724335e42e6443160448f7f
MLXProjects <maicolinux4@gmail.com>
2020-12-29 14:12:05 -0300

    Linux: fixed build.sh

--- tools/linux/build.sh
+++ tools/linux/build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+olddir=$PWD
+scriptdir=$(dirname "$(readlink -f -- "$0")")
+if [ -z "$scriptdir" ]; then
+	echo Unable to get script dir, exiting! 
+	exit
+fi
+cd $scriptdir
 ./libs/makelibs.sh
 ./build_libaroma.sh
 ./build_app.sh test
+cd $olddir
