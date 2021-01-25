33e4a9590ede7b3381b815e2d66ee43fd263a84a
MLXProjects <maicolinux4@gmail.com>
2021-01-24 22:29:16 -0300

    linux: revert envsetup.sh update (was WIP)

--- tools/linux/envsetup.sh
+++ tools/linux/envsetup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export LIBAROMA_GCC="gcc"
-export LIBAROMA_GPP="g++"
+export LIBAROMA_GCC="arm-linux-gnueabihf-gcc"
+export LIBAROMA_GPP="arm-linux-gnueabihf-g++"
 
 # export LIBAROMA_CFLAGS="-O3 -fopenmp -DLIBAROMA_CONFIG_OPENMP=1 -DLIBAROMA_CONFIG_DEBUG_TRACE=0"
-export LIBAROMA_CFLAGS="-O3 -ftree-vectorize -fprefetch-loop-arrays -funsafe-math-optimizations -fopenmp -DLIBAROMA_CONFIG_OPENMP=1 -DLIBAROMA_CONFIG_NOJPEG=1 -DLIBAROMA_CONFIG_DEBUG_TRACE=0"
+export LIBAROMA_CFLAGS="-O3 -ftree-vectorize -mvectorize-with-neon-quad -fprefetch-loop-arrays -funsafe-math-optimizations -fopenmp -mfloat-abi=hard -mfpu=neon -D__ARM_HAVE_NEON -DLIBAROMA_CONFIG_OPENMP=1 -DLIBAROMA_CONFIG_DEBUG_TRACE=0"
 export LIBAROMA_DEBUG_CFLAGS="-ggdb -fopenmp -DLIBAROMA_CONFIG_OPENMP=1 -DLIBAROMA_CONFIG_DEBUG_TRACE=0"
 
 export LIBAROMA_CONFIG_DEBUG="4"
