866f47b94712b3985e50f83afe1c74cdf6936c4a
Your Name <maicolinux4@gmail.com>
2021-03-09 19:28:58 -0300

    deploy: push res.zip to tmp instead of sdcard

--- tools/linux/recovery.sh
+++ tools/linux/recovery.sh
@@ -1,7 +1,7 @@
 olddir=$PWD
 scriptdir=$(dirname "$(readlink -f -- "$0")")
 if [ -z "$scriptdir" ]; then
-	echo Unable to get script dir, exiting! 
+	echo Unable to get script dir, exiting!
 	exit
 fi
 cd $scriptdir
@@ -10,16 +10,17 @@ if [ "$2" != "r" ]; then
 	./build_app.sh $1
 fi
 echo Waiting for device \(recovery\)
-#adb wait-for-recovery
+adb wait-for-recovery
+adb shell mkdir -p /tmp
 adb push bin/$1 /tmp
 #adb wait-for-recovery
-adb push ../../examples/$1/zip/res.zip /sdcard/
+adb push ../../examples/$1/zip/res.zip /tmp/
 #adb wait-for-recovery
 adb shell chmod 755 /tmp/$1
 #adb wait-for-recovery
 adb shell killall -19 recovery
 #adb wait-for-recovery
-adb shell /tmp/$1 0 0 /sdcard/res.zip
+adb shell /tmp/$1 0 0 /tmp/res.zip
 #adb wait-for-recovery
 adb shell killall -18 recovery
 # trap
