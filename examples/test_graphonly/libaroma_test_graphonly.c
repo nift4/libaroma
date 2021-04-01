d7924258689b37747ff743ec553a9682320d1cb9
Your Name <maicolinux4@gmail.com>
2021-04-01 17:27:52 -0300

    examples: make test_graphonly buildable

--- examples/test_graphonly/libaroma_test_graphonly.c
+++ examples/test_graphonly/libaroma_test_graphonly.c
@@ -27,15 +27,6 @@
 /* libaroma header */
 #include <aroma.h>
 
-/*
- DUMMY PNG
-*/
-#include <png.h>
-#include <pngstruct.h>
-void PNGAPI png_destroy_write_struct(png_structpp png_ptr_ptr, png_infopp info_ptr_ptr){}
-void png_free_buffer_list(png_structrp png_ptr, png_compression_bufferp *listp){}
-
-
 static LIBAROMA_CANVASP dc; /* display canvas */
 
 /* init */
@@ -56,9 +47,9 @@ byte init_aroma(){
       "file:///sdcard/Roboto-Regular.ttf"
     )
   );
-  
+
   dc=libaroma_fb()->canvas;
-  
+
   /* clean display */
   libaroma_canvas_blank(dc);
   return 1;
@@ -113,7 +104,7 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
   int item_y = list->n * list->ih;
   LIBAROMA_CANVASP cv = libaroma_canvas(list->w, list->ih*new_n);
   LIBAROMA_CANVASP cva= libaroma_canvas(list->w, list->ih*new_n);
-  
+
   /* draw previous */
   if (list->n>0){
     libaroma_draw(cv,list->cv,0,0,0);
@@ -121,14 +112,14 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
     libaroma_canvas_free(list->cv);
     libaroma_canvas_free(list->cva);
   }
-  
+
   /* draw bg */
   libaroma_draw_rect(cv, 0, item_y, list->w, list->ih, list->bgcolor, 0xff);
-  
+
   /* selected bg */
   libaroma_draw_rect(cva, 0, item_y, list->w, list->ih, list->bgcolor, 0xff);
   libaroma_draw_rect(cva, 0, item_y+libaroma_dp(1), list->w, list->ih-libaroma_dp(1), list->selcolor, 0xff);
-  
+
   char text[256];
   if (subtitle!=NULL){
     word scolor = libaroma_alpha(list->bgcolor,list->textcolor,0x66);
@@ -141,7 +132,7 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
   else{
     snprintf(text,256,"<b>%s</b>",title?title:"");
   }
-  
+
   /* draw text */
   int left_pad=libaroma_dp(72);
   int right_pad=libaroma_dp(16);
@@ -161,7 +152,7 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
     );
     libaroma_text_free(txt);
   }
-  
+
   if (icon!=NULL){
     LIBAROMA_CANVASP ico =libaroma_image_uri(icon);
     if (ico){
@@ -169,7 +160,7 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
       int icoy=item_y + ((list->ih>>1) - (dpsz>>1));
       int icox=libaroma_dp(16);
       byte ismask=(LIST_ADD_MASK_ICON_COLOR&flags)?1:0;
-        
+
       if (ismask){
         libaroma_canvas_fillcolor(ico,libaroma_alpha(list->bgcolor,list->textcolor,0xcc));
       }
@@ -191,7 +182,7 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
       libaroma_canvas_free(ico);
     }
   }
-  
+
   if (LIST_ADD_WITH_SEPARATOR&flags){
     byte is_dark = libaroma_color_isdark(list->bgcolor);
     int sepxp=0;
@@ -208,7 +199,7 @@ void list_add(MINLIST * list,char * icon, char * title, char * subtitle, byte fl
       0xff
     );
   }
-  
+
   list->cv  = cv;
   list->cva = cva;
   list->n   = new_n;
@@ -227,7 +218,7 @@ void list_show(MINLIST * list, int selectedid, int x, int y, int h){
     );
     goto syncit;
   }
-  
+
   int sel_y  = selectedid * list->ih;
   if (list->cv->h<h){
     /* dont need scroll */
@@ -239,7 +230,7 @@ void list_show(MINLIST * list, int selectedid, int x, int y, int h){
     );
     goto syncit;
   }
-  
+
   int sel_cy = sel_y + (list->ih>>1);
   int draw_y = (h>>1) - sel_cy;
   draw_y = (draw_y<0)?(0-draw_y):0;
@@ -265,7 +256,7 @@ void list_show(MINLIST * list, int selectedid, int x, int y, int h){
   /* draw indicator */
   libaroma_draw_rect(dc, x+list->w-si_w,  y+si_y, si_w-libaroma_dp(2),
     si_h, indicator_color, 120);
-  
+
 syncit:
   /* draw shadow ;) */
   libaroma_gradient_ex1(dc, x, y, list->w,libaroma_dp(5),0,0,0,0,80,0,2);
@@ -293,7 +284,7 @@ void appbar_draw(
     dc, 0, y, dc->w, h, bgcolor, 0xff
   );
   int txt_x = libaroma_dp(16);
-  
+
   if ((flags&APPBAR_FLAG_ICON_BACK)||(flags&APPBAR_FLAG_ICON_DRAWER)){
     if (flags&APPBAR_FLAG_ICON_BACK){
       libaroma_art_arrowdrawer(
@@ -316,7 +307,7 @@ void appbar_draw(
       );
     }
     txt_x = libaroma_dp((flags&APPBAR_FLAG_WIDEGAP)?72:60);
-      
+
     if (flags&APPBAR_FLAG_SELECTED){
       int sel_w=txt_x+libaroma_dp(16);
       LIBAROMA_CANVASP carea=libaroma_canvas_area(dc,0,y,sel_w,h);
@@ -329,7 +320,7 @@ void appbar_draw(
       }
     }
   }
-  
+
   LIBAROMA_TEXT txt = libaroma_text(
     text,
     textcolor,
@@ -358,7 +349,7 @@ void appbar_draw(
  * MENU DIALOG UEFI
  *
  */
- 
+
 void button_draw(const char* text, int x, int y, int w, int h) {
   word colorAccent = RGB(ff8800);
   /* draw text */
@@ -406,13 +397,13 @@ int MenuShowDialog(
   int Selection = 0;
   int MaxSelection = (Button1?1:0) + (Button2?1:0);
 
-  
+
   /* COLORS */
   word colorTextSecondary = RGB(cccccc);
   word colorTextPrimary = RGB(ffffff);
   word colorBackground = RGB(444444);
   word colorSelection = RGB(aaaaaa);
-  
+
   /*
     if(Initialized==FALSE)
       return -1;
@@ -424,7 +415,7 @@ int MenuShowDialog(
   libaroma_draw_rect(
     dc, 0, 0, dc->w, dc->h, RGB(000000), 0x7a
   );
-  
+
   /* Init Message & Title Text */
   int dialog_w = dc->w-libaroma_dp(48);
   LIBAROMA_TEXT messagetextp = libaroma_text(
@@ -456,7 +447,7 @@ int MenuShowDialog(
     libaroma_text_height(messagetextp);
   int dialog_x = libaroma_dp(24);
   int dialog_y = (dc->h>>1)-(dialog_h>>1);
-  
+
   /* draw fake shadow */
   int z;
   int shadow_sz=libaroma_dp(2);
@@ -467,12 +458,12 @@ int MenuShowDialog(
       dialog_x-z, dialog_y+(z>>1),
       dialog_w+wp, dialog_h+wp,
       0,0,
-      libaroma_dp(4), 
+      libaroma_dp(4),
       0x1111,
       shadow_opa, shadow_opa
     );
   }
-  
+
   /* draw dialog */
   libaroma_gradient(dc,
     dialog_x, dialog_y,
@@ -481,7 +472,7 @@ int MenuShowDialog(
     libaroma_dp(4), /* rounded 4dp */
     0x1111 /* all corners */
   );
-  
+
   /* draw texts */
   libaroma_text_draw(
     dc,
@@ -499,21 +490,21 @@ int MenuShowDialog(
     dialog_y+libaroma_dp(24)+libaroma_text_height(textp) + libaroma_dp(20)
   );
   libaroma_text_free(messagetextp);
-  
-  
+
+
   int i; /* i is selection */
   for (i=0;i<2;i++){
     Selection=i;
     int button_y = dialog_y+dialog_h-libaroma_dp(52);
-    
+
     /* clean button area */
     libaroma_draw_rect(dc, dialog_x, button_y, dialog_w, libaroma_dp(36), colorBackground, 0xff);
-      
+
     if(Button1) {
       /* button1 */
       int button_w = button_width(Button1);
       int button_x = dialog_x+dialog_w-button_w-libaroma_dp(16);
-      
+
       if(Selection==0) {
         libaroma_gradient_ex(dc,
           button_x,button_y,
@@ -525,7 +516,7 @@ int MenuShowDialog(
         );
       }
       button_draw(Button1, button_x, button_y-libaroma_dp(2), button_w, libaroma_dp(36));
-  
+
       /* button2 */
       if(Button2) {
         button_w = button_width(Button2);
@@ -558,7 +549,7 @@ int main(int argc, char **argv){
     int appbar_height    = libaroma_dp(56);
     int list_y           = statusbar_height + appbar_height;
     int list_height      = dc->h-list_y;
-    
+
     /*
      * DRAW STATUSBAR
      */
@@ -572,7 +563,7 @@ int main(int argc, char **argv){
     	LIBAROMA_FONT(0,3)|LIBAROMA_TEXT_CENTER,
     	100
     );
-    
+
     /*
      * Create List
      */
@@ -584,7 +575,7 @@ int main(int argc, char **argv){
       RGB(000000),
       RGB(000000)
     );
-    
+
     /*
      * Add List Items
      */
@@ -602,7 +593,7 @@ int main(int argc, char **argv){
         /*|LIST_ADD_SEPARATOR_ALIGN_TEXT*/
       );
     }
-    
+
     /* set appbar */
     appbar_draw(
       "Please Select OS",
@@ -612,13 +603,13 @@ int main(int argc, char **argv){
       appbar_height,
       APPBAR_FLAG_ICON_DRAWER|APPBAR_FLAG_SELECTED
     );
-    
+
     /* show & change list selection */
     for (i=-1;i<2;i++){
       list_show(list, i, 0, list_y, list_height);
       usleep(200000);
     }
-    
+
     /* show dialog */
     MenuShowDialog(
       "Unpatched Boot",
@@ -626,7 +617,7 @@ int main(int argc, char **argv){
       "OK",
       "CANCEL"
     );
-    
+
     /* redraw appbar & list */
     appbar_draw(
       "Please Select OS",
@@ -636,18 +627,18 @@ int main(int argc, char **argv){
       appbar_height,
       APPBAR_FLAG_ICON_DRAWER|APPBAR_FLAG_SELECTED
     );
-    
+
     /* show & change list selection */
     for (i=-1;i<30;i++){
       list_show(list, i, 0, list_y, list_height);
       usleep(200000);
     }
-    
+
     /* change appbar text */
     for (i=0;i<20;i++){
       char txapp[32];
       snprintf(txapp,32,"App Title #%i",i+1);
-      
+
       byte sflags=0;
       if (i<=10){
         if ((i%2)==0){
@@ -660,7 +651,7 @@ int main(int argc, char **argv){
           sflags|=APPBAR_FLAG_SELECTED;
         }
       }
-      
+
       appbar_draw(
         txapp,
         RGB(446688),
@@ -676,7 +667,7 @@ int main(int argc, char **argv){
       */
       usleep(300000);
     }
-    
+
     /* down to up */
     /*
     for (i=29;i>=-1;i--){
@@ -684,11 +675,11 @@ int main(int argc, char **argv){
       usleep(300000);
     }
     */
-    
+
     list_free(list);
     release_aroma();
   }
-  
+
   return 0;
 } /* End of main */
 
