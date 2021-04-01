0a416f3bc84b05aeb734483dcbfe21d02f842d9d
Your Name <maicolinux4@gmail.com>
2021-04-01 13:58:56 -0300

    linux: native.sh: don't switch to vt if platform is SDL

--- tools/linux/native.sh
+++ tools/linux/native.sh
@@ -9,14 +9,20 @@ cd $scriptdir
 if [ "$2" != "r" ]; then
 	./app.sh $1
 fi
-echo Switching to virtual terminal \(requires su\)
-# switch to virtual terminal so libaroma don't fight with X for the screen
-OLD_VT=$(sudo fgconsole)
-if [ -z $OLD_VT ]; then
-OLD_VT=7
+if [ "$LIBAROMA_PLATFORM" != "sdl" ]; then
+	# if target needs to use the entire display, switch to a vt
+	# SDL is the only which can create an X window
+	echo Switching to virtual terminal \(requires su\)
+	# switch to virtual terminal so libaroma don't fight with X for the screen
+	OLD_VT=$(sudo fgconsole)
+	if [ -z $OLD_VT ]; then
+	OLD_VT=7
+	fi
+	echo OLD_VT is $OLD_VT
+	sudo chvt 2
 fi
-echo OLD_VT is $OLD_VT
-sudo chvt 2
-echo running
+echo Starting $1
 sudo bin/$1 3 0 ../../examples/$1/zip/res.zip
-sudo chvt $OLD_VT
+if [ "$LIBAROMA_PLATFORM" != "sdl" ]; then
+	sudo chvt $OLD_VT
+fi
