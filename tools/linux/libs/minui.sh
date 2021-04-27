23ad728664149a1a9af0353c44e72db9ec9c693b
Your Name <maicolinux4@gmail.com>
2021-04-27 16:27:58 -0300

    linux: fix minui build script

--- tools/linux/libs/minui.sh
+++ tools/linux/libs/minui.sh
@@ -9,20 +9,18 @@ mkdir -p ${LIBAROMA_BASE}/tools/linux/obj/${LIBAROMA_ARCH}/minui
 cd ${LIBAROMA_BASE}/tools/linux/obj/${LIBAROMA_ARCH}/minui
 
 echo Compiling minui
-${LIBAROMA_GCC} -c \
+$LIBAROMA_GCC -c \
   -fdata-sections -ffunction-sections -Wl,--gc-sections \
   -D_GLIBCXX_DEBUG_PEDANTIC -D_GLIBCXX_DEBUG \
-  -fPIC -DPIC -Wl,-s -DUSE_MMAP -fpermissive -w \
+  -fPIC -DPIC -Wl,-s -DUSE_MMAP \
  \
-  ${LIBAROMA_CFLAGS} \
+  $LIBAROMA_CFLAGS \
  \
   	${LIBAROMA_BASE}/libs/minui/events.cpp \
     ${LIBAROMA_BASE}/libs/minui/graphics.cpp \
   	${LIBAROMA_BASE}/libs/minui/graphics_drm.cpp \
   	${LIBAROMA_BASE}/libs/minui/graphics_fbdev.cpp \
   	${LIBAROMA_BASE}/libs/minui/resources.cpp \
-  	${LIBAROMA_BASE}/libs/minui/deps/strings.cpp \
-  	${LIBAROMA_BASE}/libs/minui/deps/stringprintf.cpp \
   -I${LIBAROMA_BASE}/libs/minui \
   -I${LIBAROMA_BASE}/libs/png \
   -I${LIBAROMA_BASE}/libs/drm
